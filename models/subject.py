f8c9588af366dbd9190476f02d078d58958bebe9
yannis2 <mrcood665@gmail.com>
2026-09-04 12:27:34 -0400

    Ajout des metrics et des subjects

---
+++ models/subject.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class Sujet(ABC):
+
+    def __init__(self):
+        self._observateurs = []
+
+    def abonner(self, observateur) -> None:
+        self._observateurs.append(observateur)
+
+    def desabonner(self, observateur) -> None:
+        self._observateurs.remove(observateur)
+        
+
+    def notifier(self) -> None:
+        for o in self._observateurs:
+            o.actualiser(self)
+        
+    @abstractmethod
+    def get_donnees(self) -> dict:
+        pass
